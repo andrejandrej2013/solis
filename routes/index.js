dc2d0ca3b373824320175b9ee0c53faca845c79a
Marija Kaniveca <maria.kaniveca@gmail.com>
2022-10-03 13:13:14 +0300

    add pages

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,41 @@ var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Solis' });
 });
+// users login/reg
 router.get('/reg', function(req, res, next) {
-  res.render('reg', { title: 'Express' });
+  res.render('reg');
 });
 router.post('/reg', function(req, res) {
   console.log(req.body);
   return res.redirect('/');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.post('/login', function(req, res) {
+  console.log(req.body);
+  return res.redirect('/');
+});
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+//navigation
+router.get('/modules', function(req, res, next) {
+  res.render('modules');
+});
+router.get('/dict', function(req, res, next) {
+  res.render('dict');
+});
+router.get('/teacher', function(req, res, next) {
+  res.render('teacher');
+});
+router.get('/chat', function(req, res, next) {
+  res.render('chat');
+});
+router.get('/knowledge', function(req, res, next) {
+  res.render('knowledge');
 });
 
 module.exports = router;
