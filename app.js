9fdd012621d4ae9fee193652c483f13dd901f44d
Andrejs Grinevics <grinevic67@gmail.com>
2022-10-03 07:46:10 +0300

    new file:   _database/config/config.json 	new file:   _database/migrations/20221002110304-create-user.js 	new file:   _database/migrations/commands.txt 	new file:   _database/models/index.js 	new file:   _database/models/user.js 	new file:   _database/seeders/20221002111129-demo-user.js 	new file:   _database/seeders/commands.txt 	modified:   app.js 	new file:   controllers/user_controller 	modified:   package.json 	modified:   routes/index.js 	modified:   views/layout.pug 	new file:   views/reg.pug

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -15,7 +16,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+// import user_controller from "../controllers/user_controller";
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/reg', function(req, res, next) {
+  res.render('reg', { title: 'Express' });
+});
+router.post('/reg', function(req, res) {
+  console.log(req.body);
+  return res.redirect('/');
 });
 
 module.exports = router;
